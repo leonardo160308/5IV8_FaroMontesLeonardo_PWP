ee955c5b78ebc1c8892ae39b81fd054f6248d1a1
leonardo160308 <lfarom2300@alumno.ipn.mx>
2025-10-13 20:26:07 -0600

    se creo intermedio y subi lo de algoritmia el problea 2 que se me olvido pushearlo

--- 06Algoritmia/algoritmia.js
+++ 06Algoritmia/algoritmia.js
@@ -1,4 +1,4 @@
-function invertirPalabras() {
+function problema1() {
     const input = document.getElementById("p1-input").value.trim();
     if (input === "") {
         document.getElementById("p1-output").textContent = "Por favor, ingresa al menos una palabra.";
@@ -11,9 +11,40 @@ function invertirPalabras() {
 }
 function problema2(){
     //este es mio
+    var p2_x1=document.querySelector("#p2_x1").value;
+    var p2_x2=document.querySelector("#p2_x2").value;
+    var p2_x3=document.querySelector("#p2_x3").value;
+    var p2_x4=document.querySelector("#p2_x4").value;
+    var p2_x5=document.querySelector("#p2_x5").value;
+
+    var p2_y1 = document.querySelector("p2_y1").value;
+    var p2_y2 = document.querySelector("p2_y2").value;
+    var p2_y3 = document.querySelector("p2_y3").value;
+    var p2_y4 = document.querySelector("p2_y4").value;
+    var p2_y5 = document.querySelector("p2_y5").value;
+
+    //creeamos los vectores
+    var v1 = [p2_x1,p2_x2,p2_x3,p2_x4,p2_x5]
+    var v2 = [p2_y1,p2_y2,p2_y3,p2_y4,p2_y5]
+
+    //creamos el vector resultado
+    v1 = v1.sort(function(a,b){return b-a});
+    v2 = v2.sort(function(a,b){return b-a});
+
+    v2= v2.reverse();
+
+    var p2_producto = 0;
+    
+    for(var i=0; i< v1.length; i++){
+
+        p2_producto += v1[i]*v2[i];
+    }
+
+    document.querySelector("#p2_resultado").textContent=p2_producto;
+    
 }
 
-function palabraMasCaracteresUnicos() {
+function problema3() {
     const input = document.getElementById("p3-input").value.trim().toUpperCase();
     if (input === "") {
         document.getElementById("p3-output").textContent = "Por favor, ingresa palabras separadas por comas.";
---
+++ 07JSintermedio/intermedio.js
@@ -0,0 +1,13 @@
+/*
+js maneja variables del siguiente modo:
+
+var -> una variable de acceso local y glonbaol dependiendo de donde se declare
+let -> es una variable "prtotegida , solo se puede hacer uso dentro de la funcion o bloque donde se declara"
+cost -> es una variaboe que no puede cambiar su valor, es una constante 
+
+
+*/ 
+
+var x = "hola";
+let x ="habia una vez";
+console.log(x);
